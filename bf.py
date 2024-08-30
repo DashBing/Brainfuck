70757a1ce1ec124355cdb25c607ef55e97a0141c
DashBing <103482780+DashBing@users.noreply.github.com>
2024-08-30 12:15:04 +0800

    add Interpreter

---
+++ bf.py
@@ -0,0 +1,25 @@
+from sys import argv
+
+class IndexMinusError(Exception):
+    message = "Index can't be minus."
+    def __str__(self) -> str:
+        return self.message
+
+class Interpreter:
+    memory = [0]
+    p_now = 0
+    p_jmplist = []
+
+    def left(self):
+        if self.p_now > 0:
+            self.p_now -= 1
+        else:
+            raise IndexMinusError()
+
+    def right(self):
+        if self.p_now+2 > len(self.memory):
+            self.memory.append(0)
+        self.p_now += 1
+
+    def out(self):
+        print(chr(self.memory[self.p_now]), end="")
